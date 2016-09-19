dde3e95816ca7d893e779850fea635f5083efa46
steveafrost <ffrostt@gmail.com>
2016-09-18 21:51:23 -0400

    initial foundation css setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  get 'static/home'
+
+  get 'static/about'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: "static#index"
+  root to: "static#home"
 
   devise_for :users
 end
