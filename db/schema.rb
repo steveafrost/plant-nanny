4036b125df74b7663a125f137d6222fc79939222
steveafrost <ffrostt@gmail.com>
2016-09-19 19:35:41 -0400

    initial tip model setup

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160919231442) do
+ActiveRecord::Schema.define(version: 20160919233513) do
 
   create_table "plants", force: :cascade do |t|
     t.string   "name"
@@ -20,6 +20,12 @@ ActiveRecord::Schema.define(version: 20160919231442) do
     t.integer  "difficulty"
     t.datetime "created_at",         null: false
     t.datetime "updated_at",         null: false
+  end
+
+  create_table "tips", force: :cascade do |t|
+    t.string   "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
