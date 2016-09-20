871800e6889891cb1d2d1292bdbcc48b3d02bedb
steveafrost <ffrostt@gmail.com>
2016-09-19 21:42:11 -0400

    implemented plant form partial into plants/new and setup a before_action for find_plant method

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,4 +1,5 @@
 class PlantsController < ApplicationController
+  before_action :find_plant, except: [:index, :new, :create]
 
   def index
     @plants = Plant.all
@@ -29,5 +30,9 @@ class PlantsController < ApplicationController
 
   def plant_params
     params.require(:plant).permit(:name, :difficulty, :amount_of_light, :amount_of_water, :frequency_of_water, :fun_fact)
+  end
+
+  def find_plant
+    @plant = Plant.find(params[:id])
   end
 end
