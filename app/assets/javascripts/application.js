25b09e3c40dbd1f0680b210ff73e404de433fccb
steveafrost <ffrostt@gmail.com>
2016-09-25 11:01:29 -0400

    added bootstrap styling and js to alerts

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,16 @@
 //= require turbolinks
 //= require bootstrap-sprockets
 //= require_tree .
+
+$(function() {
+    var flashCallback;
+    flashCallback = function() {
+        return $(".alert").fadeOut();
+    };
+    $(".alert").bind('click', (function(_this) {
+        return function(ev) {
+            return $(".alert").fadeOut();
+        };
+    })(this));
+    return setTimeout(flashCallback, 2000);
+});
