0707c6b7b432560de2eb796abcebc5e8afa8a895
Steve Frost <ffrostt@gmail.com>
2016-11-24 00:33:43 -0500

    render json for plants#index and interpolate values in assembleHTML

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -3,6 +3,10 @@ class PlantsController < ApplicationController
 
   def index
     @plants = Plant.all
+    respond_to do |f|
+      f.html {render :index}
+      f.json {render json: @plants}
+    end
   end
 
   def new
