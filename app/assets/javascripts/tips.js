d19767e91c2553981c84e166cb27672e958dcc6f
steveafrost <ffrostt@gmail.com>
2016-11-25 19:00:00 -0500

    seperated js from view into seperate js files within asset pipeline

---
+++ app/assets/javascripts/tips.js
@@ -0,0 +1,13 @@
+  function loadTips(plantJSON) {
+    $('#plant-tips').empty();
+    $.each(plantJSON.tips, function(index, tip) {
+      $('#plant-tips').append(assembleTipsHTML(tip));
+    });
+  }
+
+  function assembleTipsHTML(tip) {
+    html = '<blockquote>';
+    html += `<p>${tip.content}</p>`;
+    html += '</blockquote>';
+    return html;
+  }
