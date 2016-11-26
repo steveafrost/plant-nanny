d4de2b64a593f25bbcae045354409481ee7373df
steveafrost <ffrostt@gmail.com>
2016-11-25 22:51:09 -0500

    created Tip class & createTip() method

--- app/assets/javascripts/tips.js
+++ app/assets/javascripts/tips.js
@@ -1,19 +1,21 @@
-var Tip = function(content) {
-  this.content = content;
+class Tip {
+  constructor(content, created_at) {
+    this.content = content;
+    this.timestamp = created_at;
+  }
 
-
-};
+  createTip() {
+    var html = '<blockquote>';
+    html += `<p>${this.content}</p>`;
+    html += '</blockquote>';
+    return html;
+  }
+}
 
 function loadTips(plantJSON) {
   $('#plant-tips').empty();
   $.each(plantJSON.tips, function(index, tip) {
-    $('#plant-tips').append(assembleTipsHTML(tip));
+    var tip_obj = new Tip(tip.content, tip.created_at);
+    $('#plant-tips').append(tip_obj.createTip());
   });
-}
-
-function assembleTipsHTML(tip) {
-  html = '<blockquote>';
-  html += `<p>${tip.content}</p>`;
-  html += '</blockquote>';
-  return html;
 }
