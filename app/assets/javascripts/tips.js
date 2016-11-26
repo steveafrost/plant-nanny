89bb697dc988811acba93c3d9ce44c2819a0b3b1
steveafrost <ffrostt@gmail.com>
2016-11-25 19:20:40 -0500

    created constructors for Tip and Plant in respective .js files

--- app/assets/javascripts/tips.js
+++ app/assets/javascripts/tips.js
@@ -1,13 +1,17 @@
-  function loadTips(plantJSON) {
-    $('#plant-tips').empty();
-    $.each(plantJSON.tips, function(index, tip) {
-      $('#plant-tips').append(assembleTipsHTML(tip));
-    });
-  }
+var Tip = function(content) {
+  this.content = content;
+};
 
-  function assembleTipsHTML(tip) {
-    html = '<blockquote>';
-    html += `<p>${tip.content}</p>`;
-    html += '</blockquote>';
-    return html;
-  }
+function loadTips(plantJSON) {
+  $('#plant-tips').empty();
+  $.each(plantJSON.tips, function(index, tip) {
+    $('#plant-tips').append(assembleTipsHTML(tip));
+  });
+}
+
+function assembleTipsHTML(tip) {
+  html = '<blockquote>';
+  html += `<p>${tip.content}</p>`;
+  html += '</blockquote>';
+  return html;
+}
