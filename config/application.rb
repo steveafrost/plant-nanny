482d97f63d4ca4fc2993a71b1a230aa7360659ce
Steve Frost <hello@steveafrost.com>
2017-05-30 18:48:56 -0400

    moved config.assets inside module block

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,5 @@
 require_relative 'boot'
 require 'rails/all'
-config.assets.initialize_on_precompile = false
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -12,6 +11,8 @@ module PlantNanny
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
+
+    config.assets.initialize_on_precompile = false
     config.before_configuration do
       env_file = File.join(Rails.root, 'config', 'local_env.yml')
       YAML.load(File.open(env_file)).each do |key, value|
