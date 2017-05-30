a1264d007b57c0a5d61431d5ed309410d5ef8b41
Steve Frost <hello@steveafrost.com>
2017-05-30 18:12:11 -0400

    added production secret.yml

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,6 @@
 require_relative 'boot'
-
 require 'rails/all'
+config.assets.initialize_on_precompile = false
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -21,5 +21,5 @@ module PlantNanny
   end
 end
 
-config.assets.initialize_on_precompile = false
+
 
