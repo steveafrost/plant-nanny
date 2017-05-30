72382755781e3442be562400c019ce322912a5c6
Steve Frost <hello@steveafrost.com>
2017-05-30 17:52:54 -0400

    updated gems. converted sqlite db to postgres db

--- Gemfile
+++ Gemfile
@@ -1,9 +1,10 @@
 source 'https://rubygems.org'
+ruby '2.4.0'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.0.0'
-# Use sqlite3 as the database for Active Record
-gem 'sqlite3', '~> 1.3.11'
+# Use PostGres as the database for Active Record
+gem 'pg', '~> 0.20.0'
 # Use Puma as the app server
 gem 'puma', '~> 3.0'
 # Use SCSS for stylesheets
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160928005435) do
+ActiveRecord::Schema.define(version: 20170112021122) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "documentaries", force: :cascade do |t|
+    t.string  "title"
+    t.string  "image"
+    t.string  "url"
+    t.integer "views"
+    t.boolean "timeline"
+    t.boolean "watchlist"
+  end
 
   create_table "plants", force: :cascade do |t|
     t.string   "name"
@@ -45,8 +57,8 @@ ActiveRecord::Schema.define(version: 20160928005435) do
     t.string   "last_sign_in_ip"
     t.string   "provider"
     t.string   "uid"
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
 end
